27e05c7d1afc29d06a5f49307d6571fedce78afd
5kilian <5kilian@informatik.uni-hamburg.de>
2018-11-19 22:10:14 +0100

    Add relative orientation

---
+++ src/core/orientations/RelativeOrientation.js
@@ -0,0 +1,17 @@
+import Orientation from "./Orientation";
+
+
+export default class RelativeOrientation extends Orientation {
+
+    constructor (owner, dx, dy) {
+        super (owner);
+        this.dx = dx;
+        this.dy = dy;
+    }
+
+    position () {
+        let rect = this.owner.rect();
+        return new createjs.Point(rect.x + this.dx * rect.width, rect.y + this.dy * rect.height);
+    }
+
+}
